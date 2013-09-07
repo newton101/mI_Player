7c65a49597c3dc9888efba9f7fb81dfc24146e0d
Aouled Miguil <miguil@hotmail.co.uk>
2013-09-07 11:50:45 +0100

    configure aws-s3 access keys

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MIPlayer::Application.config.secret_token = 'a3dd3911bb43e85ae7c1da0dee77219373f3222778a70384a6299004a06938d2162d68ce7debeefb092d8a529db2a3aedfcb15a518ea5886d8d43cf4e3ad6956'
